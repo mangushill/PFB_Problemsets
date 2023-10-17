70896678f2032e9555e26ed1a517076609644407
pfb16 <pfb16@info16.cshl.edu>
2023-10-17 13:10:16 -0400

    python problemset

---
+++ sys_Problem1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+My_name = sys.argv[1] # get first command line parameter
+My_favorite_color = sys.argv[2] # get second command line parameter
+My_favorite_activity = sys.argv[3]
+My_favorite_animal = sys.argv [4]
+
+# now print a message to the screen
+print(My_name,'and', My_favorite_color, 'and', My_favorite_activity, 'and', My_favorite_animal)
