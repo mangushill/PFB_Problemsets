3660eb9be286ba69649f52ac70156e93a937e273
pfb16 <pfb16@info16.cshl.edu>
2023-10-17 13:19:53 -0400

    python 1 problemset

---
+++ Problemset_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+print('My name: Melinda')
+print('My favorite color: Purple')
+activity='Kayaking'
+print('My favorite activity: ' , activity)
+print('My favorite animal: Cat')
+name= 'Melinda'
+print(f'My name: {name}')
