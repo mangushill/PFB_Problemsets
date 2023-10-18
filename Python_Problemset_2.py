8d42f9ba5115324a8e1da68732125198bbf94c7f
pfb16 <pfb16@info16.cshl.edu>
2023-10-17 20:45:59 -0400

    Python set 2 problems

---
+++ Python_Problemset_2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+#test1
+#count = 100
+#if count < 20: 
+ # message = "False is less than 20"
+ # print (count, message)
+#elif count > 20:
+ # message = "True is more than 20"
+  #print (count, message)
+
+#test2
+#count = -5
+#if count < 0:
+ # message = "True is a negative number"
+  #print (count, message)
+#elif count > 0:
+ # message = "false is a positive number"
+  #print (count, message)
+
+#test3
+#count = -5
+#if count > 0:
+ # message = "True is greater than 0"
+ # print (count, message)
+#elif count == 0:
+ # message = "true is equal to 0"
+ # print (count, message)
+#elif count < 0:
+ # message = "true is less than 0"
+  #print (count, message)
+
+#test4 tested first for 50 then for 75 to test divisiblility
+count = 75
+if count > 49:
+  message = "True is greater than 49"
+  print (count, message)
+if count % 2 == 0:
+  message = "Number is divisible by 2"
+  print (count, message)
+if count % 3 == 0:
+  message = "Number is divisible by 3"
+  print (count, message)
+
